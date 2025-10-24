26b16697c3c750b2223f46c76dc87e9c25e0c224
Mikku44 <coverblack12345@gmail.com>
2025-10-24 17:18:45 +0700

    feat : disable rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,11 +5,13 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// For extending legacy ESLint configs
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
 const eslintConfig = [
+  // Extend Next.js ESLint configs
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
@@ -19,6 +21,9 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // disable explicit any warning
+    },
   },
 ];
 
